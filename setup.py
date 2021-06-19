a832754ce958b2ef2577cc028cf8c117919f4dce
Shivathanu <gcshivathanu@gmail.com>
2021-06-19 08:22:10 +0100

    feat: configure the setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Build classification models capable of categorizing the text summary using jira data',
-    author='Shivathanu Gopirajan Chitra',
-    license='',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jira-task-classification",
+    version="0.0.1",
+    author="Shivathanu Gopirajan Chitra",
+    author_email="gcshivathanu@gmail.com",
+    description="Build classification models that enable to classify task to related categories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/promessa-project/jira-task-classification",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license='unlicense',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
 )
